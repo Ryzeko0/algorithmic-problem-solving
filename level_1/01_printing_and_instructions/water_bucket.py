3cac5c637896851c5dc7ef953945e2736edda70c
Ewen <ewen.be44@gmail.com>
2026-09-20 18:41:39 +0200

    Add water buckets exercise (Level 1)

---
+++ level_1/01_printing_and_instructions/water_bucket.py
@@ -0,0 +1,10 @@
+# Objective: Measure exactly 4 liters using 5-liter and 3-liter ungraduated buckets through successive transfers.
+
+from robot import *
+
+remplir(5)
+transferer(5, 3)
+vider(3)
+transferer(5, 3)
+remplir(5)
+transferer(5, 3)
