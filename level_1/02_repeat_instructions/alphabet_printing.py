bbaa58b91a1c03dd6d971c6ec51d406744abb09c
Ewen <ewen.be44@gmail.com>
2026-09-21 20:59:54 +0200

    Add Alphabet printing exercise (Level 1)

---
+++ level_1/02_repeat_instructions/alphabet_printing.py
@@ -0,0 +1,10 @@
+#Objective: Print the letters 'a', 'b', and 'c' followed by an underscore 30 times each on 3 separate lines.
+
+for loop in range(30):
+   print("a_", end = "")
+print("")
+for loop in range(30):
+   print("b_", end = "")
+print("")
+for loop in range(30):
+   print("c_", end = "")
